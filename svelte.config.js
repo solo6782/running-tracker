a71c582c85d9adb85ee481c1a329c3c4c24d5e01
solo6782 <solo6782@gmail.com>
2026-03-01 18:22:26 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,12 +7,3 @@ const config = {
 };
 
 export default config;
-```
-
-**Problème 2** : la commande de deploy. Dans les settings Cloudflare de ton projet, change le **Deploy command** de :
-```
-npx wrangler deploy
-```
-vers :
-```
-npx wrangler pages deploy .svelte-kit/cloudflare
